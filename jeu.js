537756e48177b1a944680977c6e6ac8dc801752b
gabriellabettonte <bettontegabriella@gmail.com>
2018-12-18 16:31:36 +0100

    Deplacement Pere Noel

--- jeu.js
+++ jeu.js
@@ -9,11 +9,38 @@ texture.onload = () => {
     context.drawImage(texture, 0 , 0, 800, 600);
 }
 
+var position=[0, 60];
+
 //Setting the initial santa claus
 pereNoel = new Image();
 pereNoel.src = 'ressources/santa.png';
 pereNoel.onload = () => {
-    context.drawImage(pereNoel, 70, 110, 70, 100, 0, 60, 20, 20);
+    context.drawImage(pereNoel, 70, 110, 70, 100, position[0], position[1], 20, 20);
+}
+
+var directions={
+	
+	"ArrowRight": [140,110, 1.5, 0],
+	"ArrowLeft": [0, 310, -1.5, 0],
+	"ArrowUp": [0,10, 0, -1.5],
+	"ArrowDown":  [0,210, 0, 1.5]
+	
+};
+
+
+
+
+document.onkeydown=function(e){
+	
+	if (directions[e.key] === undefined) {
+		return;
+	}
+	p=directions[e.key];	
+	context.clearRect(position[0],position[1], 20, 20);
+	position[0]+=p[2];
+	position[1]+=p[3];
+	context.drawImage(pereNoel, p[0], p[1], 70, 100, position[0], position[1], 20, 20);
+	
 }
 
 
